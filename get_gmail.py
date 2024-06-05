7bad5a3d810faa54de6e4d2b15876078c3332111
Thomas Loo <tomloo222@gmail.com>
2024-06-05 18:02:15 +1000

    Add notes to code

--- get_gmail.py
+++ get_gmail.py
@@ -3,32 +3,38 @@ from simplegmail.query import construct_query
 from datetime import datetime, timedelta
 import pytz
 
+# Create Gmail instance from simplegmail
 gmail = Gmail()
 
 def get_primary_emails():
-
+    # Params to only get emails received in the past day, and only in the 'primary' folder i.e. not spam or promotions folders
     query_params1 = {
         "newer_than": (1, "day"),
         "category": "primary"
     }
 
+    # Get messages that fit the query params
     messages = gmail.get_messages(query=construct_query(query_params1))
 
-    # for message in messages:
-    #     print("Subject: " + message.subject)
-
+    # Email array that will be returned
     email_array = []
 
     for message in messages:
         
+        # Get date from email
         time_string = message.date
+        # Format date to ISO format
         email_sent_time = datetime.fromisoformat(time_string)
 
+        # Find current time
         current_time = datetime.now(pytz.timezone('Australia/Sydney'))
+        # Calculate time difference
         time_diff = abs(email_sent_time - current_time)
 
+        # If email was sent in the last 30 mins, append message to email_array
         if time_diff <= timedelta(minutes=30):
             email_array.append(
+                # Email includes sender name and body text
                 f"Sender: {message.sender}"
                 f"Body: {message.plain}"
             )
--- groq_parse.py
+++ groq_parse.py
@@ -3,6 +3,7 @@ import telebot
 from dotenv import load_dotenv
 from groq import Groq
 from get_gmail import get_primary_emails
+import time
 
 load_dotenv()
 
@@ -18,6 +19,7 @@ def get_groq_response(content):
     chat_completion = client.chat.completions.create(
         messages=[
             {
+                # System prompt to Groq to summarise email content and dictate format of bot response
                 "role": "system",
                 "content": "The content sent to you is an email with 'sender' and 'body' text, return the sender's name and return a summary of the main points of this email into 30 words or less. Do not send 'Here is a 30-word or less summary of the email' prompt. Send the response with 'From: ' the sender's name only on one line, with a line break after, and then the email body summary"
             },
@@ -32,20 +34,25 @@ def get_groq_response(content):
 
 bot = telebot.TeleBot(TG_API_KEY)
 
+# Start message handler
 @bot.message_handler(commands=['start', 'help'])
 def send_start_help_message(message):
     bot.reply_to(message, "Hello I'm the Blipmail bot. Happy to be of service")
 
-
+# Any other message handler
 @bot.message_handler(func=lambda message: True, content_types=["text"])
 def all_other_messages(message):
-    # response = get_groq_response(message.text)
-    email_list = get_primary_emails()
-    for email in email_list:
-        response = get_groq_response(email)
-        bot.send_message(message.chat.id, str(response))
-    print(len(email_list))
-    # response = get_groq_response(email_list)
-    # bot.send_message(message.chat.id, str(response))
+    # Any message to the bot that is not '/start' or '/help' will trigger the 30 minute interval to check for emails
+    while True:
+        # Get email list array
+        email_list = get_primary_emails()
+        # Loop through emails in array
+        for email in email_list:
+            # Send email content to Groq
+            response = get_groq_response(email)
+            # Bot to send response from Groq
+            bot.send_message(message.chat.id, str(response))
+        
+        time.sleep(1800)
 
 bot.infinity_polling()
